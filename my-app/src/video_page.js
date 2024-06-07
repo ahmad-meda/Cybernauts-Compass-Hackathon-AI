bc5b7d00d72fb5a7f34ef5f77a875c17fb5a3a6a
an2048 <an2048@hw.ac.uk>
2024-06-07 20:09:48 +0400

    trial

---
+++ my-app/src/video_page.js
@@ -0,0 +1,15 @@
+import App from './App';
+import './App.css';
+import { FaVideo } from "react-icons/fa";
+import { FaClosedCaptioning } from "react-icons/fa";
+
+function App() {
+  return (
+    <><App></App><div className="content">
+      <textarea className="text_area" placeholder="Enter input here"></textarea>
+      <button className='generate_btn'>Generate</button>
+    </div></>
+  );
+}
+
+export default App;
